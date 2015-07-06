96ad6d9b16f892c8e5e39e44a72124a9f4ff1f05
OpenUI5 <openui5@sap.com>
2015-07-06 13:16:55 +0200

    1.28.11

--- resources/sap/ui/ux3/Collection.js
+++ resources/sap/ui/ux3/Collection.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/model/Selecti
 	 * @class
 	 * Collection
 	 * @extends sap.ui.core.Element
-	 * @version 1.28.10
+	 * @version 1.28.11
 	 *
 	 * @constructor
 	 * @public
