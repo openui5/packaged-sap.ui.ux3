89413e5e9997df019436e2538a70dc0e4df34b95
OpenUI5 <openui5@sap.com>
2015-07-21 04:34:55 +0200

    1.28.12

--- resources/sap/ui/ux3/FeedChunk.js
+++ resources/sap/ui/ux3/FeedChunk.js
@@ -24,7 +24,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/MenuButton', 'sap/ui/core/Co
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.28.11
+	 * @version 1.28.12
 	 *
 	 * @constructor
 	 * @public
