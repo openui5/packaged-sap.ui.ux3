dd990747884ca6d514042a3f42794cf02efa4890
OpenUI5 <openui5@sap.com>
2015-07-27 14:17:04 +0200

    1.30.4

--- resources/sap/ui/ux3/DataSet.js
+++ resources/sap/ui/ux3/DataSet.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/ResizeHa
 	 * @class
 	 * DataSet
 	 * @extends sap.ui.core.Control
-	 * @version 1.30.3
+	 * @version 1.30.4
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/NavigationBar.js
+++ resources/sap/ui/ux3/NavigationBar.js
@@ -25,7 +25,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/delegate
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.30.3
+	 * @version 1.30.4
 	 *
 	 * @constructor
 	 * @public
