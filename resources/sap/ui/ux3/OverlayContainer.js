c4a1f7afed8ba3c764f1d37c1a64dc07e8a15d1d
OpenUI5 <openui5@sap.com>
2015-08-31 14:25:05 +0200

    1.28.17

--- resources/sap/ui/ux3/OverlayContainer.js
+++ resources/sap/ui/ux3/OverlayContainer.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './Overlay', './library'],
 	 * @extends sap.ui.ux3.Overlay
 	 *
 	 * @author SAP SE
-	 * @version 1.28.16
+	 * @version 1.28.17
 	 *
 	 * @constructor
 	 * @public
