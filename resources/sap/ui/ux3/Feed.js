5fb7dbc5b24819d58ea94b141c55a05dfdd02159
OpenUI5 <openui5@sap.com>
2015-09-28 13:35:39 +0200

    1.26.14

--- resources/sap/ui/ux3/Feed.js
+++ resources/sap/ui/ux3/Feed.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/DropdownBox', 'sap/ui/common
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.26.13
+	 * @version 1.26.14
 	 *
 	 * @constructor
 	 * @public
