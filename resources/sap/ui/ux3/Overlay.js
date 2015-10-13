3147e9c5830c99935021becd61160bc404345381
OpenUI5 <openui5@sap.com>
2015-10-13 07:54:55 +0200

    1.28.20

--- resources/sap/ui/ux3/Overlay.js
+++ resources/sap/ui/ux3/Overlay.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/Popup', 
 	 * @implements sap.ui.core.PopupInterface
 	 *
 	 * @author SAP SE
-	 * @version 1.28.19
+	 * @version 1.28.20
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/ThingAction.js
+++ resources/sap/ui/ux3/ThingAction.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @class
 	 * Thing Action for Swatch, QuickView, Thinginspector
 	 * @extends sap.ui.core.Element
-	 * @version 1.28.19
+	 * @version 1.28.20
 	 *
 	 * @constructor
 	 * @public
