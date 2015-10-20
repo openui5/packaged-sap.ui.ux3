255d990e0aa1409fe087b4d33b9f9e011a6e3b81
OpenUI5 <openui5@sap.com>
2015-10-20 12:24:22 +0200

    1.30.10

--- resources/sap/ui/ux3/ExactList.js
+++ resources/sap/ui/ux3/ExactList.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/ListBox', 'sap/ui/core/Contr
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.30.9
+	 * @version 1.30.10
 	 *
 	 * @constructor
 	 * @public
