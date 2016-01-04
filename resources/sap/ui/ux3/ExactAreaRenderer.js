1ced5ce0f5e027c3da179752029057060f5fce71
OpenUI5 <openui5@sap.com>
2016-01-04 16:10:01 +0100

    1.34.2

--- resources/sap/ui/ux3/ExactAreaRenderer.js
+++ resources/sap/ui/ux3/ExactAreaRenderer.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2015 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2016 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -16,8 +16,8 @@ sap.ui.define(['jquery.sap.global'],
 	 */
 	var ExactAreaRenderer = {
 	};
-	
-	
+
+
 	/**
 	 * Renders the HTML for the given control, using the provided {@link sap.ui.core.RenderManager}.
 	 *
