842f3d424611063bd4f0aed315cf1beb9cf11bc0
OpenUI5 <openui5@sap.com>
2016-01-19 10:30:36 +0100

    1.32.10

--- resources/sap/ui/ux3/ExactBrowserRenderer.js
+++ resources/sap/ui/ux3/ExactBrowserRenderer.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2015 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2016 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -16,8 +16,8 @@ sap.ui.define(['jquery.sap.global'],
 	 */
 	var ExactBrowserRenderer = {
 	};
-	
-	
+
+
 	/**
 	 * Renders the HTML for the given control, using the provided {@link sap.ui.core.RenderManager}.
 	 *
@@ -37,13 +37,13 @@ sap.ui.define(['jquery.sap.global'],
 		if (oControl.getFollowUpControl()) {
 			rm.writeAttribute("aria-controls", oControl.getFollowUpControl());
 		}
-	
+
 		var sTooltip = oControl.getTooltip_AsString();
 		if (sTooltip) {
 			rm.writeAttributeEscaped("title", sTooltip);
 		}
 		rm.write(">");
-	
+
 		if (oControl.getShowHeader()) {
 			rm.write("<div class=\"sapUiUx3ExactBrwsrHd\"><h2 id=\"" + oControl.getId() + "-hdtitle\">");
 			rm.write(jQuery.sap.encodeHTML(oControl.getHeaderTitle()));
--- resources/sap/ui/ux3/OverlayDialog.js
+++ resources/sap/ui/ux3/OverlayDialog.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2015 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2016 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -18,7 +18,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/IntervalTrigger', './Overlay', 
 	 * @class
 	 * Dialog implementation based on the Overlay. If used in a Shell it leaves the Tool-Palette, Pane-Bar and Header-Items accessible.
 	 * @extends sap.ui.ux3.Overlay
-	 * @version 1.32.9
+	 * @version 1.32.10
 	 *
 	 * @constructor
 	 * @public
