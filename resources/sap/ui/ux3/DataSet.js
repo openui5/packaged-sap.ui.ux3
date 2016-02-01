4c297c9a937f45e7a5215fa66c96a26c40437da7
OpenUI5 <openui5@sap.com>
2016-02-01 14:25:29 +0100

    1.32.11

--- resources/sap/ui/ux3/DataSet.js
+++ resources/sap/ui/ux3/DataSet.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/ResizeHa
 	 * @class
 	 * DataSet
 	 * @extends sap.ui.core.Control
-	 * @version 1.32.10
+	 * @version 1.32.11
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/Feeder.js
+++ resources/sap/ui/ux3/Feeder.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/Button', 'sap/ui/core/Contro
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.32.10
+	 * @version 1.32.11
 	 *
 	 * @constructor
 	 * @public
