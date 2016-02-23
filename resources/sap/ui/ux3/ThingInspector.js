cabb3cc760c786608679e327fffebc8ce41f9ffb
OpenUI5 <openui5@sap.com>
2016-02-23 11:47:11 +0100

    1.36.3

--- resources/sap/ui/ux3/ThingInspector.js
+++ resources/sap/ui/ux3/ThingInspector.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', './ActionBar', './Overlay', './ThingViewer',
 	 * @class
 	 * Thing Inspector
 	 * @extends sap.ui.ux3.Overlay
-	 * @version 1.36.2
+	 * @version 1.36.3
 	 *
 	 * @constructor
 	 * @public
