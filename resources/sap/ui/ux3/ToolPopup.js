3d88dc8c6fc452e5fd4e1a714886889b7fa96df9
OpenUI5 <openui5@sap.com>
2016-03-21 14:36:16 +0100

    1.28.31

--- resources/sap/ui/ux3/ToolPopup.js
+++ resources/sap/ui/ux3/ToolPopup.js
@@ -27,7 +27,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/IconPool
 			 *
 			 * @namespace
 			 * @author SAP SE
-			 * @version 1.28.30
+			 * @version 1.28.31
 			 *
 			 * @constructor
 			 * @public
