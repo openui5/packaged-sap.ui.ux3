4fe5753293a3e8390c49c6abec78821c3eace310
OpenUI5 <openui5@sap.com>
2016-06-13 16:00:08 +0200

    1.36.13

--- resources/sap/ui/ux3/QuickView.js
+++ resources/sap/ui/ux3/QuickView.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/CalloutBase', 'sap/ui/core/d
 	 * @extends sap.ui.commons.CalloutBase
 	 *
 	 * @author SAP SE
-	 * @version 1.36.12
+	 * @version 1.36.13
 	 *
 	 * @constructor
 	 * @public
