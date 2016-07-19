d7f42de07d10f2f74434da106a28e4bf624524c8
OpenUI5 <openui5@sap.com>
2016-07-19 12:46:02 +0200

    1.40.1

--- resources/sap/ui/ux3/ExactArea.js
+++ resources/sap/ui/ux3/ExactArea.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/Toolbar', 'sap/ui/core/Contr
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.40.0
+	 * @version 1.40.1
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/Notifier.js
+++ resources/sap/ui/ux3/Notifier.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/Callout', 'sap/ui/core/Eleme
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.40.0
+	 * @version 1.40.1
 	 *
 	 * @constructor
 	 * @public
