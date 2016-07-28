3c6513c3ccd91df6e35cdad228eceb90ced4486b
OpenUI5 <openui5@sap.com>
2016-07-28 13:25:43 +0200

    1.38.5

--- resources/sap/ui/ux3/library.js
+++ resources/sap/ui/ux3/library.js
@@ -20,14 +20,14 @@ sap.ui.define(['jquery.sap.global',
 	 * @namespace
 	 * @name sap.ui.ux3
 	 * @author SAP SE
-	 * @version 1.38.4
+	 * @version 1.38.5
 	 * @public
 	 */
 
 	// delegate further initialization of this library to the Core
 	sap.ui.getCore().initLibrary({
 		name : "sap.ui.ux3",
-		version: "1.38.4",
+		version: "1.38.5",
 		dependencies : ["sap.ui.core","sap.ui.commons"],
 		types: [
 			"sap.ui.ux3.ActionBarSocialActions",
