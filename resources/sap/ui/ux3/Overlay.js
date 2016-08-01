11d25ed1f21f79968b41bf211288c23a1e9af33f
OpenUI5 <openui5@sap.com>
2016-08-01 11:02:50 +0200

    1.28.37

--- resources/sap/ui/ux3/Overlay.js
+++ resources/sap/ui/ux3/Overlay.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/Popup', 
 	 * @implements sap.ui.core.PopupInterface
 	 *
 	 * @author SAP SE
-	 * @version 1.28.36
+	 * @version 1.28.37
 	 *
 	 * @constructor
 	 * @public
