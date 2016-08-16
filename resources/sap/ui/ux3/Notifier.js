887f315a3530c8c49fa17c4aebb0db929cd9e0fa
OpenUI5 <openui5@sap.com>
2016-08-16 17:15:04 +0200

    1.38.7

--- resources/sap/ui/ux3/Notifier.js
+++ resources/sap/ui/ux3/Notifier.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/Callout', 'sap/ui/core/Eleme
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.38.6
+	 * @version 1.38.7
 	 *
 	 * @constructor
 	 * @public
