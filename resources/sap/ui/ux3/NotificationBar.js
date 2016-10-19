d736155a65525b8a655789f32fbdfdcd673b0665
OpenUI5 <openui5@sap.com>
2016-10-19 10:00:32 +0200

    1.40.10

--- resources/sap/ui/ux3/NotificationBar.js
+++ resources/sap/ui/ux3/NotificationBar.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/delegate
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.40.8
+	 * @version 1.40.10
 	 *
 	 * @constructor
 	 * @public
