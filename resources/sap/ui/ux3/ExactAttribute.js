af3def516115fe75d4ca016fb690043b395c7645
OpenUI5 <openui5@sap.com>
2016-12-30 09:30:57 +0100

    1.42.7

--- resources/sap/ui/ux3/ExactAttribute.js
+++ resources/sap/ui/ux3/ExactAttribute.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.42.6
+	 * @version 1.42.7
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/ToolPopup.js
+++ resources/sap/ui/ux3/ToolPopup.js
@@ -27,7 +27,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/IconPool
          *
          * @namespace
          * @author SAP SE
-         * @version 1.42.6
+         * @version 1.42.7
          *
          * @constructor
          * @public
