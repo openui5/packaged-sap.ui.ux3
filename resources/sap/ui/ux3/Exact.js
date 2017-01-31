9e08315261041d016993a240f3bd6e50c1776905
OpenUI5 <openui5@sap.com>
2017-01-31 08:04:34 +0100

    1.44.6

--- resources/sap/ui/ux3/Exact.js
+++ resources/sap/ui/ux3/Exact.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/Button', 'sap/ui/commons/Men
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.44.5
+	 * @version 1.44.6
 	 *
 	 * @constructor
 	 * @public
