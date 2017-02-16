a576bb1d43ecf7956bb2fc8690c100e4ab3bd8a4
OpenUI5 <openui5@sap.com>
2017-02-16 15:12:21 +0100

    1.46.1

--- resources/sap/ui/ux3/ExactAttribute.js
+++ resources/sap/ui/ux3/ExactAttribute.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.46.0
+	 * @version 1.46.1
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/NavigationItem.js
+++ resources/sap/ui/ux3/NavigationItem.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Item', './library'],
 	 * @extends sap.ui.core.Item
 	 *
 	 * @author SAP SE
-	 * @version 1.46.0
+	 * @version 1.46.1
 	 *
 	 * @constructor
 	 * @public
