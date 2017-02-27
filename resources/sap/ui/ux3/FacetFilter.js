1331e7482035e43b66dcd2eaecd67b7b5cd8760c
OpenUI5 <openui5@sap.com>
2017-02-27 19:43:10 +0100

    1.38.19

--- resources/sap/ui/ux3/FacetFilter.js
+++ resources/sap/ui/ux3/FacetFilter.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.38.18
+	 * @version 1.38.19
 	 *
 	 * @constructor
 	 * @public
