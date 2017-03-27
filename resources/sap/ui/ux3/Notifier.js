c154289c5acddd946f0e05ac3283d8d9739905df
OpenUI5 <openui5@sap.com>
2017-03-27 19:51:30 +0200

    1.38.20

--- resources/sap/ui/ux3/Notifier.js
+++ resources/sap/ui/ux3/Notifier.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/Callout', 'sap/ui/core/Eleme
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.38.19
+	 * @version 1.38.20
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/ToolPopup.js
+++ resources/sap/ui/ux3/ToolPopup.js
@@ -27,7 +27,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/IconPool
          *
          * @namespace
          * @author SAP SE
-         * @version 1.38.19
+         * @version 1.38.20
          *
          * @constructor
          * @public
