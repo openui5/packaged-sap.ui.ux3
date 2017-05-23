ef6c745ec209bc9c5f8091e7612b9abf6fb71a0a
OpenUI5 <openui5@sap.com>
2017-05-23 15:27:34 +0200

    1.38.22

--- resources/sap/ui/ux3/NavigationItem.js
+++ resources/sap/ui/ux3/NavigationItem.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Item', './library'],
 	 * @extends sap.ui.core.Item
 	 *
 	 * @author SAP SE
-	 * @version 1.38.21
+	 * @version 1.38.22
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/OverlayContainer.js
+++ resources/sap/ui/ux3/OverlayContainer.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './Overlay', './library'],
 	 * @extends sap.ui.ux3.Overlay
 	 *
 	 * @author SAP SE
-	 * @version 1.38.21
+	 * @version 1.38.22
 	 *
 	 * @constructor
 	 * @public
