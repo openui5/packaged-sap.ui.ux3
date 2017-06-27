f3d78f41b06fc93b4317dfd44c8c918c8e0c5c3e
OpenUI5 <openui5@sap.com>
2017-06-27 11:37:00 +0200

    1.48.3

--- resources/sap/ui/ux3/Feed.js
+++ resources/sap/ui/ux3/Feed.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/DropdownBox', 'sap/ui/common
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.48.2
+	 * @version 1.48.3
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/library.js
+++ resources/sap/ui/ux3/library.js
@@ -20,14 +20,14 @@ sap.ui.define(['jquery.sap.global',
 	 * @namespace
 	 * @name sap.ui.ux3
 	 * @author SAP SE
-	 * @version 1.48.2
+	 * @version 1.48.3
 	 * @public
 	 */
 
 	// delegate further initialization of this library to the Core
 	sap.ui.getCore().initLibrary({
 		name : "sap.ui.ux3",
-		version: "1.48.2",
+		version: "1.48.3",
 		dependencies : ["sap.ui.core","sap.ui.commons"],
 		types: [
 			"sap.ui.ux3.ActionBarSocialActions",
