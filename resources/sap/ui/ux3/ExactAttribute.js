25ec177c180eef0bf0994749ae8a8f036de0e554
OpenUI5 <openui5@sap.com>
2017-07-01 14:58:17 +0200

    1.44.16

--- resources/sap/ui/ux3/ExactAttribute.js
+++ resources/sap/ui/ux3/ExactAttribute.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.44.15
+	 * @version 1.44.16
 	 *
 	 * @constructor
 	 * @public
