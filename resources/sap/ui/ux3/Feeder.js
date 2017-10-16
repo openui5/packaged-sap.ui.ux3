5455cd3cbc209563891452bf64c8357d6c2947d6
OpenUI5 <openui5@sap.com>
2017-10-16 17:21:18 +0200

    1.50.4

--- resources/sap/ui/ux3/Feeder.js
+++ resources/sap/ui/ux3/Feeder.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/Button', 'sap/ui/core/Contro
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.50.3
+	 * @version 1.50.4
 	 *
 	 * @constructor
 	 * @public
