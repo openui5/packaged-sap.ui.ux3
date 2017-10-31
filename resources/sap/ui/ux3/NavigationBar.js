542ae3e2bd5fb57115242388659d527b8f3ccaf9
OpenUI5 <openui5@sap.com>
2017-10-31 13:48:53 +0100

    1.48.12

--- resources/sap/ui/ux3/NavigationBar.js
+++ resources/sap/ui/ux3/NavigationBar.js
@@ -25,7 +25,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/delegate
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.48.11
+	 * @version 1.48.12
 	 *
 	 * @constructor
 	 * @public
