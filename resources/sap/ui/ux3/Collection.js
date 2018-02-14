ab7c461d528f37b81b5bb4d512b6b3e7247c6b76
OpenUI5 <openui5@sap.com>
2018-02-14 14:04:31 +0100

    1.52.6

--- resources/sap/ui/ux3/Collection.js
+++ resources/sap/ui/ux3/Collection.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/model/Selecti
 	 * @class
 	 * Collection
 	 * @extends sap.ui.core.Element
-	 * @version 1.52.5
+	 * @version 1.52.6
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/CollectionInspector.js
+++ resources/sap/ui/ux3/CollectionInspector.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/delegate
 	 * @class
 	 * CollectionInspector
 	 * @extends sap.ui.core.Control
-	 * @version 1.52.5
+	 * @version 1.52.6
 	 *
 	 * @constructor
 	 * @public
