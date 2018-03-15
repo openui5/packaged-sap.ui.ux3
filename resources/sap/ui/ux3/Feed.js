e22ebc1473a8b493d24b0bee2b17988e75b55064
OpenUI5 <openui5@sap.com>
2018-03-15 18:51:35 +0100

    1.52.9

--- resources/sap/ui/ux3/Feed.js
+++ resources/sap/ui/ux3/Feed.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/DropdownBox', 'sap/ui/common
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.52.8
+	 * @version 1.52.9
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/QuickView.js
+++ resources/sap/ui/ux3/QuickView.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/CalloutBase', 'sap/ui/core/d
 	 * @extends sap.ui.commons.CalloutBase
 	 *
 	 * @author SAP SE
-	 * @version 1.52.8
+	 * @version 1.52.9
 	 *
 	 * @constructor
 	 * @public
