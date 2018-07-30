55c1eb6cdd7d7892c7aab90315ebed19835bffb1
OpenUI5 <openui5@sap.com>
2018-07-30 16:28:31 +0200

    1.56.5

--- resources/sap/ui/ux3/ThingInspector.js
+++ resources/sap/ui/ux3/ThingInspector.js
@@ -27,7 +27,7 @@ sap.ui.define([
 	 * @class
 	 * Thing Inspector
 	 * @extends sap.ui.ux3.Overlay
-	 * @version 1.56.4
+	 * @version 1.56.5
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/ToolPopup.js
+++ resources/sap/ui/ux3/ToolPopup.js
@@ -45,7 +45,7 @@ sap.ui.define([
          *
          * @namespace
          * @author SAP SE
-         * @version 1.56.4
+         * @version 1.56.5
          *
          * @constructor
          * @public
