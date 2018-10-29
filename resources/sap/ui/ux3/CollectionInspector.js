ed80c077558bfd8f48296e1d0b9755ab3d9c6bd1
OpenUI5 <openui5@sap.com>
2018-10-30 00:43:37 +0100

    1.52.21

--- resources/sap/ui/ux3/CollectionInspector.js
+++ resources/sap/ui/ux3/CollectionInspector.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/delegate
 	 * @class
 	 * CollectionInspector
 	 * @extends sap.ui.core.Control
-	 * @version 1.52.20
+	 * @version 1.52.21
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/ux3/ExactAttribute.js
+++ resources/sap/ui/ux3/ExactAttribute.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.52.20
+	 * @version 1.52.21
 	 *
 	 * @constructor
 	 * @public
