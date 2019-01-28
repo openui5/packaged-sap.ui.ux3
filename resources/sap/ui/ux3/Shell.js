bf2b60034fd3f542d550536fee3e57a145ff3662
OpenUI5 <openui5@sap.com>
2019-01-28 15:23:52 +0100

    1.56.18

--- resources/sap/ui/ux3/Shell.js
+++ resources/sap/ui/ux3/Shell.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2018 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2019 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -28,7 +28,7 @@ sap.ui.define([
 	 * The Ux3 GoldReflection Shell, which is an application frame with navigation capabilities.
 	 * It is supposed to be added to a direct child of the BODY tag of a page and there should be no other parts of the page consuming space outside the Shell.
 	 * @extends sap.ui.core.Control
-	 * @version 1.56.16
+	 * @version 1.56.18
 	 *
 	 * @constructor
 	 * @public
