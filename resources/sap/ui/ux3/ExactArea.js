b19fdff069e3ff243111bf1f39c491474199ad1a
OpenUI5 <openui5@sap.com>
2019-03-22 13:59:50 +0100

    1.44.40

--- resources/sap/ui/ux3/ExactArea.js
+++ resources/sap/ui/ux3/ExactArea.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/commons/Toolbar', 'sap/ui/core/Contr
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.44.39
+	 * @version 1.44.40
 	 *
 	 * @constructor
 	 * @public
